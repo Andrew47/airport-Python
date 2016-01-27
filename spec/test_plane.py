0e644ad843c2047c98a7360fcd5bc1576956e1ce
Andrew Burnie <apburnie@hotmail.co.uk>
2016-01-27 11:20:49 +0000

    prevents #take_off if plane already airborne

--- spec/test_plane.py
+++ spec/test_plane.py
@@ -3,6 +3,7 @@ from helper import *
 from plane import Plane
 
 class PlaneTestCase(unittest.TestCase):
+
     def setUp(self):
         self.plane = Plane()
         self.airport = MagicMock()
@@ -25,3 +26,19 @@ class PlaneTestCase(unittest.TestCase):
         self.plane.land(self.airport)
         with self.assertRaisesRegexp(Exception, 'Plane already landed'):
             self.plane.land(self.airport)
+
+    def test_not_stormy_take_off_means_airborne_true(self):
+        self.plane.land(self.airport)
+        self.plane.take_off(self.airport)
+        self.assertTrue(self.plane.is_airborne)
+
+    def test_not_stormy_take_off_calls_take_off_plane_on_airport(self):
+        self.plane.land(self.airport)
+        self.plane.take_off(self.airport)
+        self.airport.take_off_plane.assert_called_with(self.plane)
+
+    def test_not_stormy_take_off_returns_plane(self):
+        self.plane.land(self.airport)
+        self.assertEqual(self.plane.take_off(self.airport), self.plane)
+
+    

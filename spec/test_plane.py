458f58bb5d6f18bde22ff512d5af97f6508b485c
Andrew Burnie <apburnie@hotmail.co.uk>
2016-01-27 11:02:02 +0000

    adds #land to .Plane

--- spec/test_plane.py
+++ spec/test_plane.py
@@ -4,5 +4,19 @@ from plane import Plane
 
 class PlaneTestCase(unittest.TestCase):
     def setUp(self):
-        plane = Plane ()
-        airport = MagicMock ()
+        self.plane = Plane()
+        self.airport = MagicMock()
+
+    def test_plane_default_airborne(self):
+        self.assertTrue(self.plane.is_airborne)
+
+    def test_not_stormy_land_means_airborne_false(self):
+        self.plane.land(self.airport)
+        self.assertFalse(self.plane.is_airborne)
+
+    def test_not_stormy_land_returns_plane(self):
+        self.assertEqual(self.plane.land(self.airport), self.plane)
+
+    def test_not_stormy_land_calls_land_plane_on_airport(self):
+        self.plane.land(self.airport)
+        self.airport.land_plane.assert_called_with(self.plane)
